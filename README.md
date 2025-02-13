dbe1051ba815da774709f725a01779868f21614d
unknown <erickmartins2.5@hotmail.com>
2025-02-12 22:42:47 -0300

    Subindo para o repositório

---
+++ README.md
@@ -0,0 +1,1 @@
+# Exercicio Numero 1
---
+++ app.py
@@ -0,0 +1,13 @@
+n1 = input("Digite o primeiro numero: ")
+n2 = input("Digite o segundo numero: ")
+n3 = input("Digite o terceiro numero: ")
+n4 = input("Digite o quarto numero: ")
+n5 = input("Digite o quinto numero: ")
+
+
+
+soma = int(n1) + int(n2) + int(n3) + int(n4) + int(n5) / 5
+
+
+
+print(f"A média é igual a: {soma}")
